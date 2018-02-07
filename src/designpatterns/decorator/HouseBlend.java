0bf6c7581d341e5245a71e609ca221a527d602b9
Ruxandra Fediuc <ruxandra.fediuc@gmail.com>
2018-02-07 00:01:41 -0800

    Decorator - Added exercise

---
+++ src/designpatterns/decorator/HouseBlend.java
@@ -0,0 +1,11 @@
+package designpatterns.decorator;
+
+public class HouseBlend extends Beverage {
+    public HouseBlend() {
+        description = "House Blend Coffee";
+    }
+
+    public double cost() {
+        return .89;
+    }
+}
